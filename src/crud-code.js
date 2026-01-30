040c85b0d81692be8419b396548f20e6dbe6605b
Arnas-Projects <coding.myprojects@gmail.com>
2026-01-30 14:16:20 +0200

    commit

---
+++ src/crud-code.js
@@ -0,0 +1,55 @@
+
+import { v4 as uuidV4 } from 'uuid';
+
+
+
+// Programavo Marytė - Nelyst prie kodo - kodas UNIVERSALUS CRUD
+class OopCrud {
+
+    constructor(key) {
+        this.key = key;
+        this.readLocalStorage();
+    }
+
+    readLocalStorage = _ => {
+        let data = localStorage.getItem(this.key);
+
+        // ALTERNATYVA su ternary metodu
+        // null === data ? this.list = [] : this.list = JSON.parse(data);
+
+        if (null === data) {
+            this.list = [];
+        } else {
+            this.list = JSON.parse(data);
+
+        }
+    };
+
+    writeLocalStorage = _ => {
+        let data = JSON.stringify(this.list);
+        localStorage.setItem(this.key, data);
+    };
+
+    Store = data => {
+        const id = uuidV4();
+        const dataToStore = {
+            ...data,
+            id
+        }
+        this.list.unshift(dataToStore);
+        this.writeLocalStorage();
+    };
+
+    Destroy = id => {
+        this.list = this.list.filter(item => item.id != id);
+        this.writeLocalStorage();
+    };
+
+    Update = (id, data) => {
+        this.list = this.list.map(item => item.id == id ? { ...item, ...data, id } : item);
+        this.writeLocalStorage();
+    };
+}
+
+
+export default OopCrud; // eksportuojam failą Ls.js
